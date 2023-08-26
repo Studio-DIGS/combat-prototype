138400114115090315eecb9191bd742b4792696a
jucobee <jucobee@gmail.com>
2023-08-26 19:54:56 +0900

    added ability to reset enemy

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -6,20 +6,30 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Vector2 startingPosition;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        startingPosition = transform.position;
     }
     
     void Update()
     {
-        
+        if (Input.GetKeyDown("r"))
+        {
+            ResetPos();
+        }
     }
 
     public void Hit(Vector2 knockback)
     {
         Debug.Log("Got hit with " + knockback + " force");
         rb.AddForce(knockback);
+    }
+
+    public void ResetPos()
+    {
+        transform.position = startingPosition;
     }
 }
