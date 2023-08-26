97de37652129cc7b92c6eb272014e8a1b30dfd20
jucobee <jucobee@gmail.com>
2023-08-26 23:45:59 +0900

    smoke effect

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -7,11 +7,16 @@ public class EnemyController : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Vector2 startingPosition;
+    [SerializeField] private ParticleSystem particles;
+    
+    //access the particle module, in this case MainModule
+    private ParticleSystem.MainModule pMain;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         startingPosition = transform.position;
+        pMain = particles.main;
     }
     
     void Update()
@@ -20,6 +25,9 @@ public class EnemyController : MonoBehaviour
         {
             ResetPos();
         }
+
+        var emission = particles.emission;
+        emission.rateOverTime = rb.velocity.sqrMagnitude * 5f;
     }
 
     public void Hit(Vector2 knockback)
