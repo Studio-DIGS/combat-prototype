5838def7355117627e8ae2fb409ab34a932ab8c8
jucobee <jucobee@gmail.com>
2023-08-26 21:29:33 +0900

    knockback feels good

--- Assets/Player/Attacks/AttackSO.cs
+++ Assets/Player/Attacks/AttackSO.cs
@@ -8,4 +8,5 @@ public class AttackSO : ScriptableObject
     public AnimatorOverrideController animatorOV;
     public float damage;
     public Vector2 knockback;
+    [Range(0, 1)] public float freezeFrame;
 }
--- Assets/Player/PlayerCombat.cs
+++ Assets/Player/PlayerCombat.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Obscura;
 using UnityEngine;
+using UnityEngine.Rendering.VirtualTexturing;
 
 public class PlayerCombat : MonoBehaviour
 {
@@ -45,32 +46,34 @@ public class PlayerCombat : MonoBehaviour
             anim.runtimeAnimatorController = combo[comboCounter].animatorOV;
             anim.Play("Attack", 0, 0);
 
-            // Handle damage and knockback here
-            Vector2 knockback = new Vector2(combo[comboCounter].knockback.x * transform.localScale.x,
-                combo[comboCounter].knockback.y);
             Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
-            foreach (Collider2D enemy in hitEnemies)
-            {
-                enemy.GetComponent<EnemyController>().Hit(knockback);
-            }
-            
             if (hitEnemies.Length > 0)
             {
-                // Screen shake and freeze
-                if (comboCounter == combo.Count - 1)
-                {
-                    var halfTime = anim.GetCurrentAnimatorStateInfo(0).length / 2;
-                    camera.Invoke("ShakeScreen", halfTime);
-                    camera.Invoke("FreezeScreen", halfTime);
-                }
-                else
-                {
-                    camera.ShakeScreen();
-                }
+                StartCoroutine(HandleHit(hitEnemies));
             }
-
+            
             lastClickedTime = Time.time;
             comboCounter++;
+        }
+    }
+
+    IEnumerator HandleHit(Collider2D[] hitEnemies)
+    {
+        // Handle damage and knockback here
+        Vector2 knockback = new Vector2(combo[comboCounter].knockback.x * transform.localScale.x,
+            combo[comboCounter].knockback.y);
+        
+        if (comboCounter == combo.Count - 1)
+        {
+            var waitTime = anim.GetCurrentAnimatorStateInfo(0).length * combo[comboCounter].freezeFrame;
+            yield return new WaitForSeconds(waitTime);
+            camera.FreezeScreen();
+        }
+        camera.ShakeScreen();
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<EnemyController>().Hit(knockback);
         }
     }
 
